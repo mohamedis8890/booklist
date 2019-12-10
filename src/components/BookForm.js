499277f97b81d14a22ed65f4fb171524b4c637fa
Mohamed <mohamed.is8890@gmail.com>
2019-12-10 02:51:28 +0200

    Added useReducer hook

--- src/components/BookForm.js
+++ src/components/BookForm.js
@@ -4,12 +4,12 @@ import { BookContext } from "../contexts/BookContext";
 function BookForm() {
 	const [title, setTitle] = useState("");
 	const [author, setAuthor] = useState("");
-	const { addBook } = useContext(BookContext);
+	const { dispatch } = useContext(BookContext);
 
 	const handleSubmit = e => {
 		e.preventDefault();
 
-		addBook(title, author);
+		dispatch({ type: "ADD_BOOK", book: { title, author } });
 		setAuthor("");
 		setTitle("");
 	};
